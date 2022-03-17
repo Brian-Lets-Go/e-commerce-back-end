59a98d380ce3d43e7c227dfb0c4e1c927697d350
brian-lets-go <brian.w.meenan@gmail.com>
2022-03-16 22:04:51 -0400

    category get

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,11 +6,27 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: {
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }
+  }).then(data => {
+    if(!data) {
+      res.status(404).json({message : 'no data at this endpoint'});
+      return;
+    }
+    res.json(data);
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err)
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  
 });
 
 router.post('/', (req, res) => {
